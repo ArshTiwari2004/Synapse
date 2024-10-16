d353fe27bb8d421623d7d4bdfdd8f975db3d1636
Nibedan <nibedanpati@gmail.com>
2024-10-16 21:59:40 +0530

    added the firebase admin sdk hidden it too

--- Server/controllers/social.controller.js
+++ Server/controllers/social.controller.js
@@ -3,19 +3,15 @@ import User from '../models/user.model.js';
 
 export const addOrUpdateSocialMedia = async (req, res) => {
     try {
-      const userId = req.userId; // Use req.userId instead of req.user._id
+      const userId = req.userId;
       const { platform, link } = req.body;
-  
-      // Check if the social media link already exists for this user
       let socialMedia = await SocialMedia.findOne({ user: userId });
   
       if (socialMedia) {
-        // If the social media entry exists, update the link for the specific platform
         socialMedia[platform] = link;
         await socialMedia.save();
         return res.status(200).json({ message: 'Social media link updated successfully', socialMedia });
       } else {
-        // Create new social media entry for the user
         socialMedia = new SocialMedia({ user: userId, [platform]: link });
         await socialMedia.save();
         return res.status(201).json({ message: 'Social media link added successfully', socialMedia });
